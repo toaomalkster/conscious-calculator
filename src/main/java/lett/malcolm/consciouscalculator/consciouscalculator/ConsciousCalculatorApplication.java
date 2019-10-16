7f1c800dccccd07702d36e9b92293f5e3cd213d9
Malcolm Lett <grungemalcolm@gmail.com>
2019-10-16 19:24:29 +1300

    Initial import from Spring Boot Initializr

---
+++ src/main/java/lett/malcolm/consciouscalculator/consciouscalculator/ConsciousCalculatorApplication.java
@@ -0,0 +1,13 @@
+package lett.malcolm.consciouscalculator.consciouscalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsciousCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsciousCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lett/malcolm/consciouscalculator/consciouscalculator/ConsciousCalculatorApplicationTests.java
@@ -0,0 +1,16 @@
+package lett.malcolm.consciouscalculator.consciouscalculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsciousCalculatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
